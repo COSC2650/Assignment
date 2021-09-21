0fc6f84894927b2bd6bb4a315708efac31487f5a
Matt Kellock <matt@kellock.com.au>
2021-09-22 08:25:45 +1000

    Adding sample login screen and tests

--- UI/zip-it/src/App.test.tsx
+++ UI/zip-it/src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, queryByAttribute } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const getByName = queryByAttribute.bind(null, "id");
+
+test("Renders the test log in page and looks for the title", () => {
+    render(<App />);
+    expect(screen.getByText(/Log in to Zip.It/i)).toBeInTheDocument();
+});
+
+test("Renders the test log in page and looks for email field", () => {
+    const dom = render(<App />);
+    expect(getByName(dom.container, "email")).toBeInTheDocument();
+});
+
+test("Renders the test log in page and looks for the password field", () => {
+    const dom = render(<App />);
+    expect(getByName(dom.container, "password")).toBeInTheDocument();
+});
+
+test("Renders the test log in page and looks for the login button", () => {
+    const dom = render(<App />);
+    expect(getByName(dom.container, "login")).toBeInTheDocument();
+});
+
+test("Renders the test log in page and looks for the colour mode button", () => {
+    const dom = render(<App />);
+    expect(getByName(dom.container, "color_mode")).toBeInTheDocument();
 });
--- UI/zip-it/src/App.tsx
+++ UI/zip-it/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+import { Flex, Heading, Input, Button, useColorMode, useColorModeValue } from "@chakra-ui/react";
+
+const IndexPage = () => {
+    const { toggleColorMode } = useColorMode();
+    const formBackground = useColorModeValue("gray.100", "gray.700");
+    return (
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+            <Flex direction="column" background={formBackground} p={12} rounded={6}>
+                <Heading mb={6}>Log in to Zip.It</Heading>
+                <Input placeholder="your@email.com" variant="filled" mb={3} type="email" id="email" />
+                <Input placeholder="********" variant="filled" mb={6} type="password" id="password" />
+                <Button colorScheme="teal" mb={6} id="login">
+                    Log In
+                </Button>
+                <Button onClick={toggleColorMode} id="color_mode">Toggle Colour mode</Button>
+            </Flex>
+        </Flex>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <IndexPage></IndexPage>;
 }
 
 export default App;
