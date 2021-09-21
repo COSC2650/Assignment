f1f0b8046038b8105329001a95763799232936d2
David Martin <63702391+s3204581@users.noreply.github.com>
2021-09-21 20:00:05 +1000

    Create Register.cs
    Initialising registration class, testing David's git commit with gpg

---
+++ API/API/Register.cs
@@ -0,0 +1,15 @@
+namespace API
+{
+    public class Register
+    {
+        //Testing David's git commit with gpg
+        
+        //Initial registration function to  accept data from GUI
+        
+        //Call database (call to external class to return success/fail)
+        
+        //Input data into database as new fields
+        
+        //Return success/fail
+    }
+}
