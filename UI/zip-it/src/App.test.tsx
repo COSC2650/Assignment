afc0760b57e48d50e9f54f4cf37bad21beebc1c2
Matt Kellock <matt@kellock.com.au>
2021-09-22 18:26:07 +1000

    Adding S3 deployment to pipeline

--- UI/zip-it/src/App.test.tsx
+++ UI/zip-it/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen, queryByAttribute } from "@testing-library/react";
 import App from "./App";
 
-const getByName = queryByAttribute.bind(null, "id");
+const getById = queryByAttribute.bind(null, "id");
 
 test("Renders the test log in page and looks for the title", () => {
     render(<App />);
@@ -11,20 +11,20 @@ test("Renders the test log in page and looks for the title", () => {
 
 test("Renders the test log in page and looks for email field", () => {
     const dom = render(<App />);
-    expect(getByName(dom.container, "email")).toBeInTheDocument();
+    expect(getById(dom.container, "email")).toBeInTheDocument();
 });
 
 test("Renders the test log in page and looks for the password field", () => {
     const dom = render(<App />);
-    expect(getByName(dom.container, "password")).toBeInTheDocument();
+    expect(getById(dom.container, "password")).toBeInTheDocument();
 });
 
 test("Renders the test log in page and looks for the login button", () => {
     const dom = render(<App />);
-    expect(getByName(dom.container, "login")).toBeInTheDocument();
+    expect(getById(dom.container, "login")).toBeInTheDocument();
 });
 
 test("Renders the test log in page and looks for the colour mode button", () => {
     const dom = render(<App />);
-    expect(getByName(dom.container, "color_mode")).toBeInTheDocument();
+    expect(getById(dom.container, "color_mode")).toBeInTheDocument();
 });
