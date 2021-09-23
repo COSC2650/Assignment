cc59f55e2b2f758df5623c56d5d685c6547091a6
Matt Kellock <matt@kellock.com.au>
2021-09-23 13:30:10 +1000

    Adding CORS support and the Apollo GraphQL client

--- UI/zip-it/src/App.tsx
+++ UI/zip-it/src/App.tsx
@@ -1,10 +1,30 @@
 import * as React from "react";
 
 import { Flex, Heading, Input, Button, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const IndexPage = () => {
     const { toggleColorMode } = useColorMode();
     const formBackground = useColorModeValue("gray.100", "gray.700");
+    const client = new ApolloClient({
+        uri: "http://api.zipitonline.com/graphql",
+        cache: new InMemoryCache(),
+    });
+    const callGraphQL = () =>
+        client
+            .query({
+                query: gql`
+                    query {
+                        book {
+                            title
+                            author {
+                                name
+                            }
+                        }
+                    }
+                `,
+            })
+            .then((result) => console.log(result));
     return (
         <Flex height="100vh" alignItems="center" justifyContent="center">
             <Flex direction="column" background={formBackground} p={12} rounded={6}>
@@ -14,7 +34,12 @@ const IndexPage = () => {
                 <Button colorScheme="teal" mb={6} id="login">
                     Log In
                 </Button>
-                <Button onClick={toggleColorMode} id="color_mode">Toggle Colour mode</Button>
+                <Button onClick={toggleColorMode} mb={6} id="color_mode">
+                    Toggle Colour mode
+                </Button>
+                <Button onClick={callGraphQL} id="call_gql">
+                    Call GraphQL (goes to log)
+                </Button>
             </Flex>
         </Flex>
     );
