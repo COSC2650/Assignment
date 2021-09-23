e62fe9e51062eab45099145030411e26abd564f2
Matt Kellock <matt@kellock.com.au>
2021-09-23 15:10:43 +1000

    Adding CORS support and the Apollo GraphQL client

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -14,9 +14,29 @@ namespace API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var cors = System.Environment.GetEnvironmentVariable("CORS");
+            var origins = cors?.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
+
+            if (origins == null || origins.Length == 0)
+            {
+                origins = new string[] { "http://localhost:3000" };
+            }
+
             services
                 .AddGraphQLServer()
                 .AddQueryType<Query>();
+
+            services
+                .AddCors(options =>
+                {
+                    options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins(origins)
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
