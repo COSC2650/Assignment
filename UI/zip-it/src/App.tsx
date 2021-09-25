d7a2077447692154a4a8f315229239caf1c42d33
Matt Kellock <matt@kellock.com.au>
2021-09-25 12:42:55 +1000

    Cleaning up code

--- UI/zip-it/src/App.tsx
+++ UI/zip-it/src/App.tsx
@@ -3,13 +3,9 @@ import { Flex } from '@chakra-ui/layout';
 import Index from './components/forms/index';
 
 function App() {
-  const IndexPage = () => {
-    return <Index />;
-  };
-
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
-      <IndexPage></IndexPage>
+      <Index/>
     </Flex>
   );
 }
