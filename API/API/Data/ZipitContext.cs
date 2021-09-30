5aee3175c267e459acb461f04675e598c67b464f
mpbeach <s3464768@student.rmit.edu.au>
2021-09-30 16:53:21 +1000

    User Model Added Context Added EF Core Added Migration Added

---
+++ API/API/Data/ZipitContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class ZipitContext : DbContext
+    {
+        public ZipitContext(DbContextOptions<ZipitContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().HasData(
+                new User
+                {
+                    UserID = 0001,
+                    FirstName = "Bob",
+                    LastName = "Peterson",
+                    Street = "82 Fake St",
+                    City = "Fakesvile",
+                    State = "QLD",
+                    PostCode = 4114,
+                    Email = "not@real.com",
+                    PasswordHash = "hakIJJBSUBUA92912nekaso",
+                    PasswordSalt = "19373920201",
+                    EmailVerfied = true,
+                }
+            );
+        }
+    }
+}
--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using NLog;
 using Sentry.AspNetCore;
+using API.Data;
 
 namespace API
 {
@@ -12,8 +14,17 @@ namespace API
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+#if DEBUG
+        private readonly string ConnectionString = "Server=cosc2650.cl00c6onjmtb.ap-southeast-2.rds.amazonaws.com;Database=cosc2650;uid=admin;pwd=yREcSNJ3XMwPYNGVf6Py5cm75s";
+#else
+        private readonly string ConnectionString = System.Environment.GetEnvironmentVariable("CONNECTION_STRING");
+#endif
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ZipitContext>(options =>
+                options.UseMySQL(ConnectionString));
             var cors = System.Environment.GetEnvironmentVariable("CORS");
             var origins = cors?.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
 
