bc97b3248e82fb23821b7aa25756d60fce002196
Matt Kellock <matt@kellock.com.au>
2021-10-03 09:22:46 +1100

    Changing email regex

--- UI/zip-it/src/components/forms/login.tsx
+++ UI/zip-it/src/components/forms/login.tsx
@@ -31,7 +31,7 @@ export function Login(props: LoginProps) {
         };
 
         // Email regex
-        var regexp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        var regexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
 
         setFormValidationHidden(false);
 
