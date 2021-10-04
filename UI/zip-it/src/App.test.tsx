b5a50c6465552675e253940c6e41767239c8ebe1
s3632442 <you@example.com>
2021-10-05 09:21:43 +1100

    coverage added

--- UI/zip-it/src/App.test.tsx
+++ UI/zip-it/src/App.test.tsx
@@ -1,43 +1,43 @@
-import { queryByAttribute } from "@testing-library/react";
-import App from "./App";
-import { configure, mount, render } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { queryByAttribute } from '@testing-library/react';
+import App from './App';
+import { configure, mount, render } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 jest.useFakeTimers();
 
 configure({ adapter: new Adapter() });
 
-jest.mock("react", () => ({
-    ...jest.requireActual("react"),
-    useLayoutEffect: jest.requireActual("react").useEffect,
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useLayoutEffect: jest.requireActual('react').useEffect,
 }));
 
-const getById = queryByAttribute.bind(null, "id");
+const getById = queryByAttribute.bind(null, 'id');
 
-test("Should render correctly", () => {
-    // Render the component
-    const component = render(<App />);
+test('Should render correctly', () => {
+  // Render the component
+  const component = render(<App />);
 
-    // Check the component against the snapshot
-    expect(component).toMatchSnapshot();
+  // Check the component against the snapshot
+  expect(component).toMatchSnapshot();
 });
 
-test("Renders the test log in page and looks for email field", () => {
-    const dom = render(<App />);
-    // expect(getById(dom.container, "email")).toBeInTheDocument();
+test('Renders the test log in page and looks for email field', () => {
+  const dom = render(<App />);
+  // expect(getById(dom.container, "email")).toBeInTheDocument();
 });
 
-test("Renders the test log in page and looks for the password field", () => {
-    const dom = render(<App />);
-    // expect(getById(dom.container, "password")).toBeInTheDocument();
+test('Renders the test log in page and looks for the password field', () => {
+  const dom = render(<App />);
+  // expect(getById(dom.container, "password")).toBeInTheDocument();
 });
 
-test("Renders the test log in page and looks for the login button", () => {
-    const dom = render(<App />);
-    // expect(getById(dom.container, "login")).toBeInTheDocument();
+test('Renders the test log in page and looks for the login button', () => {
+  const dom = render(<App />);
+  // expect(getById(dom.container, "login")).toBeInTheDocument();
 });
 
-test("Renders the test log in page and looks for the colour mode button", () => {
-    const dom = render(<App />);
-    // expect(getById(dom.container, "color_mode")).toBeInTheDocument();
+test('Renders the test log in page and looks for the colour mode button', () => {
+  const dom = render(<App />);
+  // expect(getById(dom.container, "color_mode")).toBeInTheDocument();
 });
