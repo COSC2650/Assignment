fc1e91271c2a9b9b2401ffc71f39f4f267966a2c
s3632442 <s3632442@student.rmit.edu.au>
2021-10-12 22:02:39 +1100

    login and out button extracted

---
+++ UI/zip-it/src/components/elements/loginbutton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { IconButton, Button, Icon, useColorModeValue } from '@chakra-ui/react';
+import { FaUser } from 'react-icons/fa';
+
+interface LogInProps {
+  toggleLogIn(): void;
+}
+
+const LogInButton = (props: LogInProps) => {
+  const textColor = useColorModeValue('gray.100', 'gray.700');
+  const headerBackground = useColorModeValue('gray.700', 'gray.100');
+
+  return (
+    <>
+      <IconButton
+        onClick={props.toggleLogIn}
+        display={['block', 'none']}
+        borderColor={textColor}
+        marginLeft="5px"
+        borderWidth="1px"
+        color={textColor}
+        backgroundColor={headerBackground}
+        aria-label="Log in"
+        icon={<Icon as={FaUser} />}
+      />
+      <Button
+        onClick={props.toggleLogIn}
+        display={['none', 'block']}
+        leftIcon={<Icon as={FaUser} />}
+        borderColor={textColor}
+        marginLeft="5px"
+        borderWidth="1px"
+        color={textColor}
+        backgroundColor={headerBackground}
+        aria-label="Log in"
+      >
+        {'Log In'}
+      </Button>
+    </>
+  );
+};
+
+export default LogInButton;
