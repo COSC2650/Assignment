fd2d64b80f7320d3c29097075308a3484bda7d39
Matt Kellock <matt@kellock.com.au>
2021-10-15 10:09:59 +1100

    Fixing unsigned commits

--- UI/zip-it/src/components/forms/login.tsx
+++ UI/zip-it/src/components/forms/login.tsx
@@ -1,75 +1,115 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Input, Button, Alert, AlertIcon, AlertDescription } from "@chakra-ui/react";
-import { Flex, Spacer } from "@chakra-ui/layout";
-import { useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  Button,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  FormControl,
+} from '@chakra-ui/react';
+import { Flex, Spacer } from '@chakra-ui/layout';
+import { useState } from 'react';
 
 export interface LoginDetails {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface LoginProps {
-    onLogin(props: LoginDetails): void;
-    onOpenRegister(): void;
-    onClose(): void;
-    visible: boolean;
+  disabled: boolean;
+  onLogin(props: LoginDetails): void;
+  onOpenRegister(): void;
+  onClose(): void;
+  visible: boolean;
 }
 
 //login component
 export function Login(props: LoginProps) {
-    const [formValidationMessage, setFormValidationMessage] = useState("");
-    const [formValidationHidden, setFormValidationHidden] = useState(true);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [formValidationMessage, setFormValidationMessage] = useState('');
+  const [formValidationHidden, setFormValidationHidden] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const emailOnChange = (event) => setEmail(event.target.value);
-    const passwordOnChange = (event) => setPassword(event.target.value);
+  const emailOnChange = (event) => setEmail(event.target.value);
+  const passwordOnChange = (event) => setPassword(event.target.value);
 
-    const onLogin = () => {
-        const loginDetails: LoginDetails = {
-            email: email,
-            password: password,
-        };
+  const onLogin = () => {
+    const loginDetails: LoginDetails = {
+      email: email,
+      password: password,
+    };
 
-        // Email regex
-        var regexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
+    // Email regex
+    var regexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
 
-        setFormValidationHidden(false);
+    setFormValidationHidden(false);
 
-        if (email === "" || !regexp.test(email)) {
-            setFormValidationMessage("Your email is empty or invalid");
-        } else if (password === "") {
-            setFormValidationMessage("Your passwork is empty");
-        } else {
-            setFormValidationHidden(true);
-            props.onLogin(loginDetails);
-        }
+    if (email === '' || !regexp.test(email)) {
+      setFormValidationMessage('Your email is empty or invalid');
+    } else if (password === '') {
+      setFormValidationMessage('Your passwork is empty');
+    } else {
+      setFormValidationHidden(true);
+      props.onLogin(loginDetails);
     }
+  };
 
-    return (
-        <Modal isOpen={props.visible} onClose={props.onClose} id="login">
-            <ModalOverlay />
-            <ModalContent>
-                <ModalHeader>Ready? Set? Zip-It!</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                    <Alert status="error" hidden={formValidationHidden} mb={3}>
-                        <AlertIcon />
-                        <AlertDescription>{formValidationMessage}</AlertDescription>
-                    </Alert>
-                    <Input onChange={emailOnChange} placeholder="your@email.com" variant="filled" mb={3} type="email" id="email" />
-                    <Input onChange={passwordOnChange} placeholder="********" variant="filled" type="password" id="password" />
-                </ModalBody>
+  return (
+    <FormControl>
+      <Modal isOpen={props.visible} onClose={props.onClose} id="login">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Ready? Set? Zip-It!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Alert status="error" hidden={formValidationHidden} mb={3}>
+              <AlertIcon />
+              <AlertDescription>{formValidationMessage}</AlertDescription>
+            </Alert>
+            <Input
+              disabled={props.disabled}
+              onChange={emailOnChange}
+              placeholder="your@email.com"
+              variant="filled"
+              mb={3}
+              type="email"
+              id="email"
+            />
+            <Input
+              disabled={props.disabled}
+              onChange={passwordOnChange}
+              placeholder="********"
+              variant="filled"
+              type="password"
+              id="password"
+            />
+          </ModalBody>
 
-                <ModalFooter>
-                    <Flex width="100%">
-                        <Button onClick={props.onOpenRegister} id="register">Register</Button>
-                        <Spacer></Spacer>
-                        <Button onClick={onLogin} id="login">Log In</Button>
-                    </Flex>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
-    );
+          <ModalFooter>
+            <Flex width="100%">
+              <Button
+                onClick={props.onOpenRegister}
+                id="register"
+                disabled={props.disabled}
+              >
+                Register
+              </Button>
+              <Spacer></Spacer>
+              <Button onClick={onLogin} id="login" disabled={props.disabled}>
+                Log In
+              </Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </FormControl>
+  );
 }
 
 export default Login;
---
+++ UI/zip-it/src/components/forms/logout.test.tsx
@@ -0,0 +1,97 @@
+import { Logout, LogoutProps } from "./logout";
+import { configure, mount, render } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+jest.useFakeTimers();
+
+configure({ adapter: new Adapter() });
+
+jest.mock("react", () => ({
+    ...jest.requireActual("react"),
+    useLayoutEffect: jest.requireActual("react").useEffect,
+}));
+
+describe("LogOut", () => {
+    it("Should render correctly", () => {
+        const props: LogoutProps = {
+            onLogout: () => {
+                // Intentional
+            },
+            onClose: () => {
+                // Intentional
+            },
+            visible: true,
+        };
+
+        const component = render(<Logout {...props} />);
+        expect(component).toMatchSnapshot();
+    });
+
+    it("Visibility should change", () => {
+        const props: LogoutProps = {
+            onLogout: () => {
+                // Intentional
+            },
+            onClose: () => {
+                // Intentional
+            },
+            visible: false,
+        };
+
+        // Render the component in a hidden state
+        const wrapper = mount(<Logout {...props} />);
+
+        // Check that it is not visible
+        expect(wrapper.find("AlertDialog").prop("isOpen")).toEqual(false);
+
+        // Change the component to a visible state
+        wrapper.setProps({ visible: true });
+
+        // Check that it is visible
+        expect(wrapper.find("AlertDialog").prop("isOpen")).toEqual(true);
+    });
+
+    it("onClose method should be triggered", () => {
+        const mockLogoutCallBack = jest.fn();
+        const mockOnCloseCallBack = jest.fn();
+
+        const props: LogoutProps = {
+            onLogout: mockLogoutCallBack,
+            onClose: mockOnCloseCallBack,
+            visible: true,
+        };
+
+        // Render the component in a hidden state
+        const wrapper = mount(<Logout {...props} />);
+
+        wrapper.find("button#close").simulate("click");
+
+        // Check that the callback was called
+        expect(mockOnCloseCallBack.mock.calls.length).toEqual(1);
+
+        // Check that none of the other callbacks were called
+        expect(mockLogoutCallBack.mock.calls.length).toEqual(0);
+    });
+
+    it("onLogout method should be triggered", () => {
+        const mockLogoutCallBack = jest.fn();
+        const mockOnCloseCallBack = jest.fn();
+
+        const props: LogoutProps = {
+            onLogout: mockLogoutCallBack,
+            onClose: mockOnCloseCallBack,
+            visible: true,
+        };
+
+        // Render the component in a hidden state
+        const wrapper = mount(<Logout {...props} />);
+
+        wrapper.find("button#log_out").simulate("click");
+
+        // Check that the callback was called
+        expect(mockLogoutCallBack.mock.calls.length).toEqual(1);
+
+        // Check that none of the other callbacks were called
+        expect(mockOnCloseCallBack.mock.calls.length).toEqual(0);
+    });
+});
