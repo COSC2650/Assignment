513798960b7c8cc7c5dbcf8faec041d45bb72f64
s3632442 <s3632442@student.rmit.edu.au>
2021-10-20 11:00:39 +1100

    Implement conditional rendering

--- UI/zip-it/src/data/queries.tsx
+++ UI/zip-it/src/data/queries.tsx
@@ -1,15 +1,78 @@
-import { gql } from "@apollo/client";
-const query = (email: string, password: string) => {
-    return {
-        query: gql`
-      {
-        userByEmail(email: "${email}", password: "${password}") {
-          email
-          firstName
-        }
-      }
-    `,
-    };
-};
+import { gql } from '@apollo/client';
 
+const query = (props) => {
+  if (props!= null) {
+    console.log('userQuery--------------------------------------');
+    return {
+      query: gql`
+    {
+      userByEmail(email: "${props.email}", password: "${props.password}") {
+        email
+        firstName
+      }
+    }
+  `,
+  };
+  } if(props.postcode > 0){
+    console.log("postcode query -------------------------------------");
+    return {
+      query: gql`
+        {
+          ads{
+            title
+          }
+        }
+      `,
+    }
+
+  }
+  if(props.type === "product"){
+    console.log("product query --------------------------------------");
+    return {
+      query: gql`
+        {
+          ads{
+            title
+          }
+        }
+      `,
+    }
+
+  }if(props.type === "service"){
+    console.log("service query --------------------------------------");
+    return {
+      query: gql`
+        {
+          ads{
+            title
+          }
+        }
+      `,
+    }
+
+  }if(props.type === "onLoad"&&props.category==="onLoad"){
+    console.log("query on load --------------------------------------");
+    return {
+      query: gql`
+        {
+          ads{
+            title
+          }
+        }
+      `,
+    }
+
+  }else {
+    console.log('Query Undefined -- Zip-It---------------------------');
+    return {
+      query: gql`
+        {
+          ads{
+            title
+          }
+        }
+      `,
+    };
+  }
+};
 export default query;
