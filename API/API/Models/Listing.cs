3c526fe6949f302942e52c0da99b06e8e7435ae8
mpbeach <s3464768@student.rmit.edu.au>
2021-10-20 18:35:46 +1000

    model updates

---
+++ API/API/Models/Listing.cs
@@ -0,0 +1,41 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class Listing
+    {
+        [Key]
+        public int ListingID { get; set; }
+
+        [Required]
+        public int UserID {get; set; }
+        public virtual User User { get; set; }
+
+        [Required, MinLength(4), MaxLength(4)]
+        public int PostCode { get; set; }
+
+        [Required, MinLength(3)]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime DateListed { get; set; }
+
+        [Required, MinLength(3)]
+        public string Category { get; set; }
+
+        [Required]
+        public Decimal Price { get; set; }
+
+        [Required, MinLength(3)]
+        public string ListingType { get; set; }
+
+        [Required, MinLength(3)]
+        public string Description { get; set; }
+
+        public string ProdCondition { get; set; }
+
+        public DateTime ServAvailability { get; set; }
+    }
+}
