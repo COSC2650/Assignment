bcb898db45f635f5ceddcdfbd12d063f86fac045
s3632442 <s3632442@student.rmit.edu.au>
2021-10-25 11:56:47 +1100

    removed redundant query

--- UI/zip-it/src/data/queries.tsx
+++ UI/zip-it/src/data/queries.tsx
@@ -14,29 +14,9 @@ const query = (props) => {
     }
   `,
     };
-  }
-  if (
-    props.listingPostcode !== 0 ||
-    props.listingType !== "" ||
-    props.listingCategory !== ""
-  ) {
-    console.log("listings query --------------------------------------");
-    return {
-      query: gql`
-        {
-          listingsByFilter(listingPostcode:${props.listingPostcode},listingType:"${props.listingType}",category:"${props.listingCategory}") {
-            listingID
-            title
-            description
-            imageURL
-            listingType
-          }
-        }
-      `,
-    };
-  } else {
+  }else {
     //called when no query defined for the passed params
-    console.log("Default query -- Zip-It");
+    console.log("Listings query -- Zip-It");
     return {
       query: gql`
       {
