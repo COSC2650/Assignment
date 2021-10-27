4c962a331337c4a1a43e9c488fa0c1b7ff85af2b
Matt Kellock <matt@kellock.com.au>
2021-10-27 14:15:08 +1100

    Adding API functionality

--- API/API/Extensions/Mailer.cs
+++ API/API/Extensions/Mailer.cs
@@ -5,31 +5,26 @@ namespace API.Extensions
 {
     public class Mailer
     {
-        private readonly string _email;
         private readonly ISmtpClient _client;
 
-        public Mailer(string email, ISmtpClient client)
+        public Mailer(ISmtpClient client)
         {
-            _email = email;
             _client = client;
         }
 
-        public bool SendMail() {
+        public bool SendRegistrationMail(string email, int confirmationCode) {
             bool result = true;
 
             //VERIFY EMAIL USING AMAZON SES
             MailAddress from = new("nonotrespond@zipitonline.com");
 
             //ADDRESS OF THE CODE RECIPIENT
-            MailAddress to = new(_email);
-
+            MailAddress to = new(email);
             MailMessage message = new(from, to);
 
             message.Subject ="Confirmation Code";
 
-            message.Body = "Welcome to Zip It Online!";
-            message.Body = "Please enter the confirmation code below to confirm your account";
-            message.Body = ConfirmCodeGenerator();
+            message.Body = String.Concat("Welcome to Zip It Online!", "\n\n", "Please enter the confirmation code below to confirm your account: ", confirmationCode.ToString());
             message.BodyEncoding =  System.Text.Encoding.UTF8;
             message.Subject = "Zip It Online Confirmation";
             message.SubjectEncoding = System.Text.Encoding.UTF8;
@@ -41,10 +36,6 @@ namespace API.Extensions
             Console.WriteLine("Email sent!");
 
             return result;
-        }
-
-        public string ConfirmCodeGenerator(){
-            return System.Security.Cryptography.RandomNumberGenerator.GetInt32(0, int.MaxValue).ToString();
         }
     }
 }
--- API/API/Services/UserServices.cs
+++ API/API/Services/UserServices.cs
@@ -1,6 +1,6 @@
-using System.Linq;  
-using System.Threading.Tasks;  
-using Microsoft.EntityFrameworkCore;  
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Models;
 using API.Extensions;
@@ -8,58 +8,99 @@ using System;
 using API.GraphQL.Users;
 
 namespace API.Services
-{  
-    public class UserService : IUserService  
-    {  
+{
+    public class UserService : IUserService
+    {
         private readonly ZipitContext _context;
-    
-        public UserService(ZipitContext context)  
-        {  
+
+        public UserService(ZipitContext context)
+        {
             _context = context;
-        } 
-  
-        public async Task<User> CreateUser(AddUserInput input)
-        {  
-            var hash = Hashbrowns.HashPassword(input.UserPassword);
-            var user = new User
-            {
-                UserFirstName = input.UserFirstName,
-                UserLastName = input.UserLastName,
-                UserStreet = input.UserStreet,
-                UserCity = input.UserCity,
-                UserState = input.UserState,
-                UserPostCode = input.UserPostCode,
-                UserEmail = input.UserEmail,
-                UserPasswordHash = hash,
-                UserEmailVerfied = false,
-                RoleID = 2
-            };
-
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return user;
         }
 
-        public async Task<bool> DeleteUser(int UserID)  
-        {  
-            var user = await  _context.Users.FirstOrDefaultAsync(c => c.UserID == UserID);
+        public async Task<User> CreateUser(AddUserInput input)
+        {
+            // Create a return object
+            User result;
+
+            if (_context.Users.Where(x => x.UserEmail == input.UserEmail).Any())
+            {
+                // If a user already exists, then return a 0
+                result = new()
+                {
+                    UserID = 0
+                };
+            }
+            else
+            {
+                // Create a new confirmation code
+                int confirmationCode = CodeGenerator.ConfirmCodeGenerator();
+
+                // Hash the password
+                var hash = Hashbrowns.HashPassword(input.UserPassword);
+
+                // Create a new instance of the user
+                var user = new User
+                {
+                    UserFirstName = input.UserFirstName,
+                    UserLastName = input.UserLastName,
+                    UserStreet = input.UserStreet,
+                    UserCity = input.UserCity,
+                    UserState = input.UserState,
+                    UserPostCode = input.UserPostCode,
+                    UserEmail = input.UserEmail,
+                    UserPasswordHash = hash,
+                    UserEmailVerfied = false,
+                    RoleID = 2
+                };
+
+                // Add the user to the context
+                _context.Users.Add(user);
+
+                // Add a new instance of the confirmation code
+                ConfirmCode confirmCode = new()
+                {
+                    Email = input.UserEmail,
+                    Code = confirmationCode
+                };
+
+                // Add the confirmtion code to the context
+                _context.ConfirmCodes.Add(confirmCode);
+
+                // Send the confirmation mailer
+                Mailer mailer = new(new SmtpClient());
+                mailer.SendRegistrationMail(input.UserEmail, confirmationCode);
+
+                // Save the changes to the data store
+                await _context.SaveChangesAsync();
+
+                // After the user object is added, grab it back
+                result = _context.Users.Where(x => x.UserEmail == input.UserEmail).First();
+            }
+
+            // Return the result
+            return result;
+        }
+
+        public async Task<bool> DeleteUser(int UserID)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(c => c.UserID == UserID);
             var response = false;
 
-            if(user is not null)   
-            {  
-                _context.Users.Remove(user);  
-                await _context.SaveChangesAsync();  
-                response = true;  
+            if (user is not null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                response = true;
             }
 
             return response;
         }
 
-        public IQueryable<User> GetAll()  
-        {  
+        public IQueryable<User> GetAll()
+        {
             var users = _context.Users.AsQueryable();
-            foreach(User user in users)
+            foreach (User user in users)
             {
                 user.UserPasswordHash = "#";
             }
@@ -69,13 +110,13 @@ namespace API.Services
         public async Task<User> GetUserByEmail(string email, string password)
         {
             var user = await _context.Users.FirstOrDefaultAsync(c => c.UserEmail == email);
-            if(user is null)
+            if (user is null)
                 return null;
-            
+
             var result = ValidatePassword(user, password);
             if (!result)
                 return null;
-            
+
             user.UserPasswordHash = "#";
             return user;
         }
