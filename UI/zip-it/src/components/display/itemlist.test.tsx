6bae2674c2c8b5b9b6c5a096ea65bebac0999d70
s3632442 <s3632442@student.rmit.edu.au>
2021-10-28 11:04:57 +1100

    cleaned up

--- UI/zip-it/src/components/display/itemlist.test.tsx
+++ UI/zip-it/src/components/display/itemlist.test.tsx
@@ -1,20 +1,19 @@
-import { configure, mount, render } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import ListItem, { ListItemProp } from "../../components/elements/listitem";
-import Listings from './itemlist';
+import { configure, render } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Listings from "./itemlist";
 
 jest.useFakeTimers();
 
 configure({ adapter: new Adapter() });
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useLayoutEffect: jest.requireActual('react').useEffect,
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useLayoutEffect: jest.requireActual("react").useEffect,
 }));
 
-describe('Item List', () => {
-    it('should match snapshot', () => {
-const componenet = render (<Listings />);
-expect(componenet).toMatchSnapshot();
-    })
-})
+describe("Item List", () => {
+  it("should match snapshot", () => {
+    const componenet = render(<Listings />);
+    expect(componenet).toMatchSnapshot();
+  });
+});
