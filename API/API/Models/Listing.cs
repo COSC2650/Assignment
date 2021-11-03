e0363c9ca94adb5f7e119af3d1cc7628d9984319
mpbeach <s3464768@student.rmit.edu.au>
2021-11-03 13:23:05 +1000

    old Listing.cs inserted from main

--- API/API/Models/Listing.cs
+++ API/API/Models/Listing.cs
@@ -10,25 +10,28 @@ namespace API.Models
         public int ListingID { get; set; }
 
         [Required]
-        public int UserID { get; set; }
+        public int UserID {get; set; }
         public virtual User User { get; set; }
 
         [Required, MinLength(4), MaxLength(4)]
         public int ListingPostCode { get; set; }
 
-        [Required]
+        [Required, MinLength(3)]
         public string ListingTitle { get; set; }
 
+        [Required]
         public DateTime ListingDate { get; set; }
 
+        [Required, MinLength(3)]
         public string ListingCategory { get; set; }
 
         [Required]
         public Decimal ListingPrice { get; set; }
 
+        [Required, MinLength(3)]
         public string ListingType { get; set; }
 
-        [Required]
+        [Required, MinLength(3)]
         public string ListingDescription { get; set; }
 
         public string ListingCondition { get; set; }
