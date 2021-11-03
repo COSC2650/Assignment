280946b59a6fa109aa163c591667c8262bfa203e
David <s3204581@student.rmit.edu.au>
2021-11-03 12:08:38 +1100

    API query stuffarounds

--- API/API/Models/Listing.cs
+++ API/API/Models/Listing.cs
@@ -10,28 +10,24 @@ namespace API.Models
         public int ListingID { get; set; }
 
         [Required]
-        public int UserID {get; set; }
-        public virtual User User { get; set; }
+        public int ListingUserID { get; set; }
 
         [Required, MinLength(4), MaxLength(4)]
         public int ListingPostCode { get; set; }
 
-        [Required, MinLength(3)]
+        [Required]
         public string ListingTitle { get; set; }
 
-        [Required]
         public DateTime ListingDate { get; set; }
 
-        [Required, MinLength(3)]
         public string ListingCategory { get; set; }
 
         [Required]
         public Decimal ListingPrice { get; set; }
 
-        [Required, MinLength(3)]
         public string ListingType { get; set; }
 
-        [Required, MinLength(3)]
+        [Required]
         public string ListingDescription { get; set; }
 
         public string ListingCondition { get; set; }
