4d503eba0e58654f5f7632047e46cc9ad14f78fa
s3632442 <s3632442@student.rmit.edu.au>
2021-11-23 01:15:09 +1100

    pr time

--- UI/zip-it/src/components/elements/listitem.tsx
+++ UI/zip-it/src/components/elements/listitem.tsx
@@ -33,53 +33,44 @@ const ListItem = (props: ListItemProp) => {
   const { isOpen,onOpen, onClose } = useDisclosure();
 
   return (
-    <>
-      <HStack align="flex-start" width="100%" onClick={onOpen}>
-        <HStack align="left">
-          <Heading as="h1" size="md" id="heading">
-            {props.listingTitle}
-          </Heading>
-          <Text size="md" id="contents">
-            {props.listingDescription}
-          </Text>
-          <Text size="md" id="postcode">
-            {'Postcode - ' + props.listingPostCode}
-          </Text>
-          <Text size="md" id="price">
-            {'Price - ' + props.listingPrice}
-          </Text>
+      <>
+        <HStack align="flex-start" width="100%" onClick={onOpen}>
+          <HStack align="left">
+            <Heading as="h1" size="md" id="heading">
+              {props.listingTitle}
+            </Heading>
+            <Text size="md" id="contents">
+              {props.listingDescription}
+            </Text>
+            <Text size="md" id="postcode">
+              {'Postcode - ' + props.listingPostCode}
+            </Text>
+            <Text size="md" id="price">
+              {'Price - ' + props.listingPrice}
+            </Text>
+          </HStack>
         </HStack>
-      </HStack>  
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        >
-            <DrawerOverlay />
-            <DrawerContent>
-                <DrawerCloseButton />
-                <DrawerHeader>{props.listingTitle}</DrawerHeader>
+        <Drawer isOpen={isOpen} placement="bottom" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader>{props.listingTitle}</DrawerHeader>
 
-                <DrawerBody>
-                <Text> {'Post Code - ' +props.listingPostCode}</Text>
-                <Text>{'Category- ' +props.listingCategory}</Text>
-                <Text>{'Condition - ' +props.listingCondition}</Text>
-                <Text>{'Availability - ' +props.listingAvailibility}</Text>
-                <Text>{'Description: ' +props.listingDescription}</Text>
-                <Text>{'Price - ' + props.listingPrice}</Text>
-                <Text>{'Type - ' +props.listingType}</Text>
+            <DrawerBody>
+              <Text> {'Post Code - ' + props.listingPostCode}</Text>
+              <Text>{'Category- ' + props.listingCategory}</Text>
+              <Text>{'Condition - ' + props.listingCondition}</Text>
+              <Text>{'Availability - ' + props.listingAvailibility}</Text>
+              <Text>{'Description: ' + props.listingDescription}</Text>
+              <Text>{'Price - ' + props.listingPrice}</Text>
+              <Text>{'Type - ' + props.listingType}</Text>
+            </DrawerBody>
 
-                </DrawerBody>
-
-                <DrawerFooter>
-                    <Button variant="outline" mr={3} onClick={onClose}>
-                        Cancel
-                    </Button>
-                    <Button colorScheme="blue">Save</Button>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer></>
-  );
+            <DrawerFooter>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </>
+    );
 };
 
 
