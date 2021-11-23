07de77e97c12516c72555a5aa4e6978a0441f3b8
s3632442 <s3632442@student.rmit.edu.au>
2021-11-23 13:24:54 +1100

    removed console log

--- UI/zip-it/src/data/queries.tsx
+++ UI/zip-it/src/data/queries.tsx
@@ -2,7 +2,6 @@ import { gql } from '@apollo/client';
 
 //fetches user authentication information
 const query = (props) => {
-  console.log(props);
   if (props.email != null) {
     return {
       query: gql`
